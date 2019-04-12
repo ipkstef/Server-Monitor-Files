812656153d540313bf085db4613c72f145628534
ipkstef <stefano.amanuel@me.com>
2019-04-12 05:15:45 -0400

    security fix

--- monitor.sh
+++ monitor.sh
@@ -7,7 +7,7 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/usr/bin/curl
 PMESSAGE=$(check_docker_container.sh pokecraft)
 
 
-curl --silent 'https://api.twilio.com/2010-04-01/Accounts/AC108acf465f89df469fd0de6d7d669e64/Messages.json' -X POST \
+curl --silent 'https://api.twilio.com/2010-04-01/Accounts/$AuthSid/Messages.json' -X POST \
 --data-urlencode 'To=+17038324707' \
 --data-urlencode 'From=+17034579188' \
 --data-urlencode "Body='$PMESSAGE'" \

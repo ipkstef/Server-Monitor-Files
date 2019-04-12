df2d7153af1d5afb55001bd91a1ef47409cacf17
ipkstef <stefano.amanuel@me.com>
2019-04-12 01:37:05 -0400

    cleaned up code and added env variables

--- monitor.sh
+++ monitor.sh
@@ -1,7 +1,9 @@
-now=$(check_docker_container.sh 9c5858ea1772)
+#define variable for running container 
+pmessage=$(check_docker_container.sh pokecraft)
+
 
 curl 'https://api.twilio.com/2010-04-01/Accounts/AC108acf465f89df469fd0de6d7d669e64/Messages.json' -X POST \
 --data-urlencode 'To=+17038324707' \
 --data-urlencode 'From=+17034579188' \
---data-urlencode "Body='$now'" \
--u AC108acf465f89df469fd0de6d7d669e64:4328a809a7ba94ed686dacb2b63d72c0
+--data-urlencode "Body='$pmessage'" \
+-u $TWILLO_API
